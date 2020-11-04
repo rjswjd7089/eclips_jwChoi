4e78f21ae06c7a39e67e8ce661d806710dd1c22b
wku <wku@192.168.172.147>
2020-11-04 15:45:39 +0900

    rodong

--- Helloworld/src/First/Helloworld.java
+++ Helloworld/src/First/Helloworld.java
@@ -5,8 +5,8 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("HyeonJin, Gwak");
-		System.out.println("20192719");
+		System.out.println("JeongWon, Choi");
+		System.out.println("20173163");
 		System.out.println("Computer SW engineering");
 	}
 	
